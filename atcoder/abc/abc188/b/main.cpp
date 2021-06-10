d46b1e240ebf7bb10d3b90f6de2c4ea22b880727
sugapiko2 <sugapiko2@gmail.com>
2021-06-11 02:56:47 +0900

    abc188

---
+++ atcoder/abc/abc188/b/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep(i, n) for (int64_t i = 0; i < (int64_t)(n); i++)
+using vi = vector<int64_t>;
+using vvi = vector<vi>;
+using ll = int64_t;
+using P = pair<ll,ll>;
+ll INF = 1000000007;
+// ll INF = 9223372036854775807;
+// ll INF = 998244353;
+
+int main() {
+    // fasten cin
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // implement
+    ll n;
+    cin >> n;
+    vi a(n), b(n);
+    rep(i,n) cin >> a.at(i);
+    rep(i,n) cin >> b.at(i);
+    ll ip = 0;
+    rep(i,n) ip += a.at(i) * b.at(i);
+    cout << (ip == 0 ? "Yes" : "No") << endl;
+}
