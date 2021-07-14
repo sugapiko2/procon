2512f1a99398ec7dfc96891d2f1f192c9cb23e37
sugapiko2 <sugapiko2@gmail.com>
2021-07-14 23:11:03 +0900

    abc200c

---
+++ atcoder/abc/abc200/c/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep_(i, m_, n_, m, n, ...) for (int64_t i = m; i < n; i++)
+#define rep(i, ...) rep_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+using ll = int64_t;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using P = pair<ll, ll>;
+ll INF = 1000000007;
+// ll INF = 9223372036854775807;
+// ll INF = 998244353;
+
+int main()
+{
+    // fasten cin
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    // implement
+    ll n;
+    vi a(n);
+    rep(i, n) cin >> a.at(i);
+    // 200で割ったあまりが同じ組み合わせを探す
+    vi nums(201, 0);
+    rep(i, n) nums.at(a.at(i) % 200);
+    ll res = 0;
+    rep(i, 201)
+    {
+        ll num = nums.at(i);
+        res += (num * (num - 1)) / 2;
+    }
+    cout << res << endl;
+}
